69d13f8ca8155b1769b8160fca01e1d3207b8fc7
hakiemaul <noersedya@gmail.com>
2017-06-05 10:43:25 +0700

    moved all js to local

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
+var cors = require('cors');
 const express = require('express');
 var app = express()
 const bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/ecommerce');
-var cors = require('cors')
 
 var books = require('./routes/books');
 var customers = require('./routes/customers');
 var transactions = require('./routes/transactions');
 var index = require('./routes/index')
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
