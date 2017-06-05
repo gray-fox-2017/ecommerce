d97bb39b3375ff1117646b04e826048aec6e5930
raynormw <tirtawiryaputra@yahoo.com>
2017-06-05 20:06:26 +0700

    fixing login

--- client/assets/login.js
+++ client/assets/login.js
@@ -1,23 +1,29 @@
 $("#login-button").click(function(event) {
-  event.preventDefault();
-  axios.post('http://localhost:3000/customers/login', {username: $("#username").val(), password: $("#password").val() })
-  .then ((res) => {
-    if (res == '/') {
-      setTimeout(function() {
-      //alert('login failed');
+  let username = $("#username").val();
+  let password = $("#password").val();
+
+  if (username != "" && password != "") {
+    event.preventDefault();
+    axios.post('http://localhost:3000/customers/login', {username: username, password: password})
+    .then ((res) => {
+      if (res.data == '/') {
+        alert('login failed, check your username and password');
+        setTimeout(function() {
+        window.location.href = './index.html';
+        }, 1000);
+      } else {
+        $('form').fadeOut(500);
+        $('.wrapper').addClass('form-success');
+        setTimeout(function(){
+        window.location.href = './main.html';
+        },3000);
+      }
+    })
+    .catch ((err) => {
+      console.log('failed');
+      console.log(err.message);
       window.location.href = './index.html';
-      },3000);
-    } else {
-      $('form').fadeOut(500);
-      $('.wrapper').addClass('form-success');
-      setTimeout(function(){
-      window.location.href = './main.html';
-      },3000);
-    }
-  })
-  .catch ((err) => {
-    console.log('failed');
-    console.log(err.message);
-    window.location.href = './index.html';
-  });
+    });
+  }
+
 });
